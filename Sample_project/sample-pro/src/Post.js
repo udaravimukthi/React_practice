bdaba540c94c221ccf9e0aa4b098ece0a90cc07d
Udara Vimukthi <uvlakshan@gmail.com>
2021-11-15 02:05:46 +0530

    Added new compnent named Post with its css

---
+++ Sample_project/sample-pro/src/Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Post.css';
+class Post extends Component{
+    render(){
+        return(
+            <div className="Post">
+                <h2>Post</h2>
+            </div>
+
+        );
+    }
+}
+
+export default Post;
