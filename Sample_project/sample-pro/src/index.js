3d12eb190ebaec8771b2af2e9f2aada9b0c714dc
Udara Vimukthi <uvlakshan@gmail.com>
2021-11-15 02:05:15 +0530

    Added changes in main index

--- Sample_project/sample-pro/src/index.js
+++ Sample_project/sample-pro/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Post from './Post';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <App />
+    <Post/>
   </React.StrictMode>,
   document.getElementById('root')
 );
